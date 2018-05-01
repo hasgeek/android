448b4baad273ae9a3b74c3d24ab3de4acb970477
Arnav Gupta <arnav@codingblocks.com>
2018-05-01 19:57:53 +0530

    do not hardcode to only 2017 events
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- app/src/main/java/com/hasgeek/funnel/spaces/SpacesActivity.java
+++ app/src/main/java/com/hasgeek/funnel/spaces/SpacesActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
